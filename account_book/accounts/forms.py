3f25341d82b4e42c409896b0b9fa76306ff26ebe
s-nakano <nanaedashokudai@gmail.com>
2017-10-16 00:20:52 +0900

    AdminLTE2を追加 それに合わせて各画面を調整中

--- account_book/accounts/forms.py
+++ account_book/accounts/forms.py
@@ -3,6 +3,16 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 
 
+class LoginForm(AuthenticationForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['placeholder'] = 'ユーザー名(ログイン名)'
+        self.fields['password'].widget.attrs['class'] = 'form-control'
+        self.fields['password'].widget.attrs['placeholder'] = 'パスワード'
+
+
 class RegisterForm(UserCreationForm):
 
     class Meta:
@@ -29,11 +39,3 @@ class RegisterForm(UserCreationForm):
 
         self.fields['password2'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['placeholder'] = 'パスワード（確認）'
-
-
-class LoginForm(AuthenticationForm):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs['class'] = 'form-control'
-        self.fields['password'].widget.attrs['class'] = 'form-control'
