eeaecab8629cbb574a60f460feb16a99dc5e84da
s-nakano <nanaedashokudai@gmail.com>
2017-11-03 19:53:21 +0900

    ちょい修正

--- account_book/cms/views/index.py
+++ account_book/cms/views/index.py
@@ -8,10 +8,7 @@ class IndexView(CommonTemplateView):
     def get_context_data(self, **kwargs):
 
         context = super().get_context_data(**kwargs)
-        # domain = 'http://192.168.10.5:8000/'
-        domain = 'http://192.168.10.20/'
         context = {
-            'link_admin': domain + 'admin/login/',
             'link_code': 'https://github.com/douke03/account_book/tree/master/account_book',
         }
         return context
