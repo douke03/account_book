c165e2d06fea36d2fac7a45fa7572b65353fa953
s-nakano <nanaedashokudai@gmail.com>
2017-11-12 14:41:32 +0900

    全ページ用の共通処理を追加

---
+++ account_book/cms/context_processors.py
@@ -0,0 +1,11 @@
+"""contet_processor."""
+from cms.models.todo_model import ToDo
+
+
+def common_process(request):
+    context = {
+        'link_code': 'https://github.com/douke03/account_book/tree/master/account_book',
+        'link_AdminLTE': 'https://adminlte.io/themes/AdminLTE/index.html',
+        'test': ToDo.objects.filter(is_complete=False).count(),
+    }
+    return context
--- account_book/cms/views/index.py
+++ account_book/cms/views/index.py
@@ -4,11 +4,3 @@ from cms.views.common_view import CommonTemplateView
 class IndexView(CommonTemplateView):
 
     template_name = 'index.html'
-
-    def get_context_data(self, **kwargs):
-
-        context = super().get_context_data(**kwargs)
-        context = {
-            'link_code': 'https://github.com/douke03/account_book/tree/master/account_book',
-        }
-        return context
