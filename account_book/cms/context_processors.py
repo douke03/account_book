17b55d5390352f7159bc59fa019556dc9d8523ff
s-nakano <nanaedashokudai@gmail.com>
2017-11-28 00:27:10 +0900

    iroiro

--- account_book/cms/context_processors.py
+++ account_book/cms/context_processors.py
@@ -3,12 +3,20 @@ from cms.models.todo_model import ToDo
 
 def common_process(request):
 
-    unfinished_task_cnt = ToDo.objects.filter(is_complete=False).count()
-
     try:
-        unfinished_task = ToDo.objects.filter(
-            is_active=True, is_complete=False).order_by('priority', 'created_at')[:1].get()
+        if request.user.is_superuser:
+            unfinished_task_cnt = ToDo.objects.filter(
+                is_complete=False).count()
+            unfinished_task = ToDo.objects.filter(
+                is_active=True, is_complete=False).order_by('priority', 'created_at')[:1].get()
+        else:
+            unfinished_task_cnt = ToDo.objects.filter(
+                created_by=request.user, is_complete=False).count()
+            unfinished_task = ToDo.objects.filter(
+                created_by=request.user, is_active=True, is_complete=False).order_by(
+                'priority', 'created_at')[:1].get()
     except:
+        unfinished_task_cnt = 0
         unfinished_task = None
 
     context = {
